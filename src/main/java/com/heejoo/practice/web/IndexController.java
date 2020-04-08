68d6afcc3de62840c0694fa6a999aab1519fe532
imkimheejoo <jujudamdadi@naver.com>
2020-04-09 03:30:23 +0900

    add: 글 수정 페이지 추가

--- src/main/java/com/heejoo/practice/web/IndexController.java
+++ src/main/java/com/heejoo/practice/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.heejoo.practice.web;
 
 import com.heejoo.practice.service.PostsService;
+import com.heejoo.practice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsSave(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -5,6 +5,9 @@ const index = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
     },
     save: function () {
         var data = {
@@ -25,7 +28,30 @@ const index = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         })
+    },
+
+    update: function() {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
     }
+
 };
 
 index.init();
