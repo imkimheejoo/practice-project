beae719c51f36ddd5ce1ca6b3170a0e08fd4bab3
imkimheejoo <jujudamdadi@naver.com>
2020-04-11 00:04:44 +0900

    add: 회원 권한 enum, 회원 repo 추가

--- src/main/java/com/heejoo/practice/domain/user/User.java
+++ src/main/java/com/heejoo/practice/domain/user/User.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.management.relation.Role;
 import javax.persistence.*;
 
 @Getter
---
+++ src/main/java/com/heejoo/practice/domain/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.heejoo.practice.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
