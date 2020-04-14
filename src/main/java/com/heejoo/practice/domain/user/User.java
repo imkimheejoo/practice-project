f30672d0c1e638b91ef0767fb352ca3609bbfe38
imkimheejoo <jujudamdadi@naver.com>
2020-04-15 08:12:10 +0900

    add: User 도메인에 BaseTimeEntity 상속

--- src/main/java/com/heejoo/practice/domain/user/User.java
+++ src/main/java/com/heejoo/practice/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.heejoo.practice.domain.user;
 
+import com.heejoo.practice.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class User {
+public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
